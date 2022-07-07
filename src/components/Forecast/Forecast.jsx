599116e12c80d32f5434afff33abe130e85dbf5d
rrich-kray <rrich.kray@gmail.com>
2022-07-07 11:09:31 -0400

    added daily tiles to right-hand panel

--- src/components/Forecast/Forecast.jsx
+++ src/components/Forecast/Forecast.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./Forecast.css"
+import { format_date, format_date_day, date_shortform } from '../../utils/helpers';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
@@ -11,25 +12,27 @@ const Forecast = ({ weatherData }) => {
                 <span>This Week</span>
                 <ArrowForwardIosIcon />
             </div>
-            <div className="today">
+            {/* <div className="today">
                 <h5>Today</h5>
                 <div className="today-tiles flex-row justify-center align-center">
-                    {/* {weatherData.data.hourly.map(hour => (
+                    {weatherData.data.hourly.map(hour => (
                         <div className="today-tile"></div>
-                    ))} */}
+                    ))}
                 </div>
-            </div>
-            <div className="forecast-panel">
+            </div> */}
+            <div className="forecast-panel flex-col justify-between">
                 {weatherData && weatherData.data.daily.map(day => (
                     <div className="forecast-tile flex-row justify-between align-center">
-                        <div className="forecast-container">
-                            <span></span>
-                            <span></span>
+                        <div className="forecast-container flex-col align-start justify-center">
+                            <span>{format_date_day(day.dt)}</span>
+                            <span>{date_shortform(day.dt)}</span>
                         </div>
-                        <div className="forecast-temp">
-                            <span>{day.temp.day}</span>
+                        <div className="forecast-temp flex-row align-center justify-center">
+                            <span>{`${day.temp.day} °F`}</span>
                         </div>
-                        <div className="forecast-icon"></div>
+                        <div className="forecast-icon flex-row justify-end">
+                            <img src={`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`} alt="" />
+                        </div>
                     </div>
                 ))}
             </div>
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,5 @@
+// All functions herein assume input is UNIX format
+
 export const format_time = (s) => {
   const dtFormat = new Intl.DateTimeFormat("en-US", {
     timeStyle: "medium",
@@ -5,4 +7,21 @@ export const format_time = (s) => {
   });
 
   return dtFormat.format(new Date(s * 1e3));
+};
+
+export const format_date_day = (timestamp) => {
+  const dateObj = new Date(timestamp * 1000);
+  return dateObj.toLocaleString("en-US", { weekday: "long" });
+};
+
+export const format_date = (timestamp) => {
+  const dateObj = new Date(timestamp * 1000);
+  return dateObj.toLocaleDateString();
+};
+
+export const date_shortform = (timestamp) => {
+  const dateObj = new Date(timestamp * 1000);
+  return `${dateObj
+    .toLocaleString("en-US", { month: "long" })
+    .slice(0, 3)} ${dateObj.toLocaleString("en-US", { day: "numeric" })}`;
 };
